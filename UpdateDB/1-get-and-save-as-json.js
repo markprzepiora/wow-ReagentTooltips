f3b66bf36389ecd7d327ddd09107848f0a9ac8fa
Mark Przepiora <mark.przepiora@gmail.com>
2019-01-05 15:36:17 -0700

    Add UpdateDB scripts for updating DB

---
+++ UpdateDB/1-get-and-save-as-json.js
@@ -0,0 +1,55 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const curry = require('curry');
+
+function looseJsonParse(obj){
+  return Function('"use strict";return (' + obj + ')')();
+}
+
+async function getReagents(url) {
+  let res = await fetch(url);
+  let html = await res.text();
+
+  let listViewItemsJSMatch = html.match(/var listviewitems = (.*);\n/);
+
+  if (!listViewItemsJSMatch) {
+    throw "response did not contain a listviewitems array";
+  }
+
+  let listViewItemsJS = listViewItemsJSMatch[1];
+  let listViewItems = looseJsonParse(listViewItemsJS);
+
+  return listViewItems;
+}
+
+const wrapReagents = curry(function wrapReagents(name, list) {
+  return { name, list };
+});
+
+async function getAndWrapReagents(name, url) {
+  return await getReagents(url).then(wrapReagents(name));
+}
+
+async function getAndSaveAll() {
+  let promises = [
+    [ "alchemy",         "https://www.wowhead.com/items?filter=217:87;1:1;0:0" ],
+    [ "blacksmithing",   "https://www.wowhead.com/items?filter=217:87;1:2;0:0" ],
+    [ "cooking",         "https://www.wowhead.com/items?filter=217:87;1:3;0:0" ],
+    [ "enchanting",      "https://www.wowhead.com/enchanting-trade-goods" ],
+    [ "engineering",     "https://www.wowhead.com/items?filter=217:87;1:5;0:0" ],
+    [ "inscription",     "https://www.wowhead.com/items?filter=217:87;1:15;0:0" ],
+    [ "jewelcrafting",   "https://www.wowhead.com/jewelcrafting-trade-goods" ],
+    [ "leatherworking",  "https://www.wowhead.com/items?filter=217:87;1:8;0:0" ],
+    [ "mining",          "https://www.wowhead.com/items?filter=217:87;1:9;0:0" ],
+    [ "tailoring",       "https://www.wowhead.com/items?filter=217:87;1:10;0:0" ],
+  ].map(([name, url]) => getAndWrapReagents(name, url));
+
+  let all = await Promise.all(promises);
+
+  all.forEach(({ name, list }) => {
+    console.log(name);
+    fs.writeFileSync(`${__dirname}/items-json/${name}.json`, JSON.stringify(list, null, 2));
+  });
+}
+
+getAndSaveAll();
---
+++ UpdateDB/2-convert-to-lua.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+let files = [
+  { jsonFileName: "alchemy.json",         luaName: "Alchemy" },
+  { jsonFileName: "blacksmithing.json",   luaName: "Blacksmithing" },
+  { jsonFileName: "cooking.json",         luaName: "Cooking" },
+  { jsonFileName: "enchanting.json",      luaName: "Enchanting" },
+  { jsonFileName: "engineering.json",     luaName: "Engineering" },
+  { jsonFileName: "inscription.json",     luaName: "Inscription" },
+  { jsonFileName: "jewelcrafting.json",   luaName: "Jewelcrafting" },
+  { jsonFileName: "leatherworking.json",  luaName: "Leatherworking" },
+  { jsonFileName: "mining.json",          luaName: "Mining" },
+  { jsonFileName: "tailoring.json",       luaName: "Tailoring" },
+];
+
+files.forEach(({ jsonFileName, luaName }) => {
+  let items = JSON.parse(fs.readFileSync(`${__dirname}/items-json/${jsonFileName}`));
+  let luaItemLines = items.map(item => {
+    const name = (item.name || "").replace(/^[0-9]/, '');
+    return `  ${item.id}, --${name}`;
+  });
+
+  let lua = `--[[
+  Data Parsed from WowHead.com
+]]
+
+local ReagentHelper2 = ReagentHelper2;
+
+ReagentHelper2.${luaName} = {
+${luaItemLines.join("\n")}
+}
+`;
+  let outPath = path.join(__dirname, '..', 'Data', 'DB', 'Profession', `${luaName}.lua`);
+  fs.writeFileSync(outPath, lua);
+});
